4ca3f1bc42dff6e3f2b3a84b260f5c08acdde868
Jimmy Quach <jgquach@ucsd.edu>
2016-02-16 23:28:21 -0800

    analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,15 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$(".likeCtr").click(projectClick);
+}
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event  
+    console.log("hi");    
+    ga("send", "event", "like", "click");
+    console.log("bye");
 }
