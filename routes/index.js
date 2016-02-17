756b04237c1e1362b97fc1fddf70aee926847432
Jimmy Quach <jgquach@ucsd.edu>
2016-02-17 00:15:13 -0800

    add coin flip?

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,27 @@ var projects = require('../projects.json');
  * GET home page.
  */
 
+
 exports.view = function(req, res){
 	projects["grid"] = false;
   	res.render('index', projects);
 };
 
+exports.view = function(req, res){
+	var random_num = Math.random();
+	console.log(random_num);
+
+	if (random_num > 0.5){
+		projects["grid"] = false;
+		res.render('index', projects);
+	}
+	else{
+		projects["grid"] = true;
+		res.redirect('/grid');
+	}
+};
+
 exports.viewGrid = function(req, res){
 	projects["grid"] = true;
-  	res.render('index', projects);
+	res.render('index', projects);
 };
